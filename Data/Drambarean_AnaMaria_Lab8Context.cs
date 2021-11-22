017ef0771a45092451ba59707d03c1a60148a220
Ana Drambarean <anamariadrambarean@gmail.com>
2021-11-22 13:34:07 +0000

    Add project files.

---
+++ Data/Drambarean_AnaMaria_Lab8Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Drambarean_AnaMaria_Lab8.Models;
+
+namespace Drambarean_AnaMaria_Lab8.Data
+{
+    public class Drambarean_AnaMaria_Lab8Context : DbContext
+    {
+        public Drambarean_AnaMaria_Lab8Context (DbContextOptions<Drambarean_AnaMaria_Lab8Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Drambarean_AnaMaria_Lab8.Models.Book> Book { get; set; }
+
+        public DbSet<Drambarean_AnaMaria_Lab8.Models.Publisher> Publisher { get; set; }
+
+        public DbSet<Drambarean_AnaMaria_Lab8.Models.Category> Category { get; set; }
+    }
+}
---
+++ Pages/Books/Index.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Drambarean_AnaMaria_Lab8.Data;
+using Drambarean_AnaMaria_Lab8.Models;
+
+namespace Drambarean_AnaMaria_Lab8.Pages.Books
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Drambarean_AnaMaria_Lab8.Data.Drambarean_AnaMaria_Lab8Context _context;
+
+        public IndexModel(Drambarean_AnaMaria_Lab8.Data.Drambarean_AnaMaria_Lab8Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Book> Book { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Book = await _context.Book
+                .Include(b => b.Publisher)
+                .ToListAsync();
+        }
+    }
+}
